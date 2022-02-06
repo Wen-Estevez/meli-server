3f47ed5f5e00d79734e47f03638fb1f9676825e4
Wen-Estevez <westevez04@gmail.com>
2022-02-06 13:53:09 -0500

    Puerto para heroku agregado

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require('./src/app.js');
 
-// Syncing all the models at once.
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
