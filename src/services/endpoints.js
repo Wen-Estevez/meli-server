c3fc36ae6c56945f8d03a81e6e534a8f12467c51
Wen-Estevez <westevez04@gmail.com>
2022-02-06 12:43:51 -0500

    Respuesta corregida

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,29 +1,31 @@
+const axios = require('axios');
+
 module.exports = {
     meliQuery: async function (query) {
         try {
-            const search = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
-            return search;
+            const search = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
+            return search.data;
         }
         catch (e) {
-            console("Error en la query " + e);
+            console.log("Error en la query " + e);
         }
     },
     meliParamId: async function (id) {
         try {
-            const search = await fetch(`https://api.mercadolibre.com/items/${id}`)
-            return search;
+            const search = await axios.get(`https://api.mercadolibre.com/items/${id}`)
+            return search.data;
         }
         catch (e) {
-            console("Error en la busqueda por id " + e);
+            console.log("Error en la busqueda por id " + e);
         }
     },
     meliDescriptionId: async function (id) {
         try {
-            const search = await fetch(`https://api.mercadolibre.com/items/${id}/description`)
-            return search;
+            const search = await axios.get(`https://api.mercadolibre.com/items/${id}/description`)
+            return search.data;
         }
         catch (e) {
-            console("Error en la busqueda de descripcion" + e);
+            console.log("Error en la busqueda de descripcion" + e);
         }
     }
 };
